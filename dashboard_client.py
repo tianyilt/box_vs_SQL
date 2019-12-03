74814773d477895287e35b552c08ee5e42e47dd3
朱いぜん <55439233+syuizendesu@users.noreply.github.com>
2019-12-03 14:25:24 +0800

    我又贡献了几行代码

---
+++ dashboard_client.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter.messagebox as tm
+#def dashboard_client():
+dashboard = Tk()
+dashboard.title("dashboard")
+dashboard.geometry("350x350")
+c = Canvas(dashboard, width=350, height=350)
+c.pack()
+dashboard.mainloop()
---
+++ dashboard_managementengine.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter.messagebox as tm
+#def management_engine():
+dme = Tk()
+dme.title("dme_svc")
+dme.geometry("1280x720")
+c = Canvas(dme, width=1280, height=720)
+c.pack()
+dme.mainloop()
