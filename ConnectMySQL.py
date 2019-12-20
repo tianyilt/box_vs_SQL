77de75437634624339b689db37cdff2daefdc6f1
tianyilt <tianyilt@qq.com>
2019-12-20 19:40:52 +0800

    Update ConnectMySQL.py

--- ConnectMySQL.py
+++ ConnectMySQL.py
@@ -3,32 +3,22 @@ import pymysql
 这只是一个链接数据库的demo
 """
 db = pymysql.connect('182.254.217.138', 'ZNDY', 'ZNDY@ecust123', 'box_vs_sql', charset="utf8")
-sur = db.cursor()
 
-sur.execute('SET NAMES utf8;')
-sur.execute('SET CHARACTER SET utf8;')
-sur.execute('SET character_set_connection=utf8;')
-sur.execute("drop table if exists Question")
-sql_create = """create table Question (person char(200),question varchar(200))"""
-sur.execute(sql_create)
+cursor = db.cursor()
 
-sql = """CREATE TABLE EMPLOYEE (
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT,  
-         SEX CHAR(1),
-         INCOME FLOAT )"""
-sur.execute(sql)
+# SQL 插入语句
+sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+       LAST_NAME, AGE, SEX, INCOME) \
+       VALUES ('%s', '%s',  %s,  '%s',  %s)" % \
+       ('Mac', 'Mohan', 20, 'M', 2000)
 
-db.commit()
-while nt:
-    data = getData(soup)
-    for item in data:
-        sqlinsert = """insert into Question(person,question) values ('%s','%s')""" % (
-        pymysql.escape_string(item[1]), pymysql.escape_string(item[0]))
-        sur.execute(sqlinsert)
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 如果发生错误则回滚
+   db.rollback()
 
-    soup = getSoup(url + nt, headers)
-    nt = nextUrl(soup)
-db.commit()
 db.close()
