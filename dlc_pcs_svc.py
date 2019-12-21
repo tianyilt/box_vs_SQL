e5fbd8611c6f4504667fe49e184a176a7565039a
tianyilt <tianyilt@qq.com>
2019-12-22 00:31:35 +0800

    不花钱购买dlc的时代来临了

--- dlc_pcs_svc.py
+++ dlc_pcs_svc.py
@@ -4,28 +4,40 @@ import tkinter.messagebox as tm
 from tkinter.ttk import *
 def dlc(uid):
     def confirmpurchase():
-        contentchange=contentshowup.curselection()
+        contentchange=contentshowup.curselection()#tuple,tuple[0]is dlc code
         if contentchange != ():
-            if tm.askyesno("ご確認ください",contentchange+"の購入を確認しますか？")==1:
-               tm.showinfo("メッセージ",contentchange+"購入成功！")
+            dlc_dic = {0: 'free', 1: 'conquest', 2: 'european', 3: 'future', 4: 'midevil', 5: 'woody'}  # 提醒自己还没做
+            if tm.askyesno("ご確認ください",dlc_dic[contentchange[0]]+"の購入を確認しますか？")==1:
+               tm.showinfo("メッセージ",dlc_dic[contentchange[0]]+"購入成功！")
 
                ########################contentchange写入数据库
                 #购买后,相关记录变更
 
-               # import pymysql
-               # db = pymysql.connect('182.254.217.138', 'ZNDY', 'ZNDY@ecust123', 'box_vs_sql', charset="utf8")
-               # cursor = db.cursor()
-               #
-               # sql = """select PassWord from login where UserName ='%s' """ % (inputusrname)
-               #
-               # try:
-               #     cursor.execute(sql)  # 执行sql语句
-               #
-               # except Exception as e:
-               #     db.rollback()
-               #
-               # finally:
-               #     db.close()
+               import pymysql
+               import datetime
+               dt = datetime.datetime.now()#get current time
+               dt_now = dt.strftime('%Y-%m-%d %H:%M:%S')
+
+               db = pymysql.connect('182.254.217.138', 'ZNDY', 'ZNDY@ecust123', 'box_vs_sql', charset="utf8")
+               cursor = db.cursor()
+               sql1="""select UID from login where UserName ='%s' """ % (uid)
+
+               try:
+                   cursor.execute(sql1)  # 执行sql语句
+                   result=cursor.fetchall()
+                   if result:
+                       sql2 = """INSERT INTO dlc(Dtype, UID, \
+                              money, date) \
+                              VALUES ('%s', '%s',%d , '%s')""" % \
+                              (contentchange[0], result[0][0], 6, dt_now)
+                       cursor.execute(sql2)
+                       db.commit()
+
+               except Exception as e:
+                   db.rollback()
+
+               finally:
+                   db.close()
 
 
             else:
@@ -37,7 +49,6 @@ def dlc(uid):
         """
         contentshowup.delete(0,END)
         #['free', 'conquest', 'european', 'future', 'midevil', 'woody']是主题
-        dlc_dic={0:'free', 1:'conquest', 2:'european', 3:'future', 4:'midevil', 5:'woody'}#提醒自己还没做
         dlc_purchase_state={0:'未购买',1:'已拥有'}
         dlc_purchase_flag_list=[1,0,0,0,0,0]#第i个元素表示第i个dlc购买状态，0没买1买了
     #################################拉取数据库视图
--- select_inf.sql
+++ select_inf.sql
@@ -11,7 +11,7 @@ FROM      login CROSS JOIN
                 dlc
 WHERE   (login.uid = dlc.UID) AND (login.username = 'tianyilt')
 
-
-
+insert into dlc values
+(1,1,1,20,'2019-12-03 18:00:01');
 
 show processlist ;
