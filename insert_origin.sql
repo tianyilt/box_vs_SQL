6e6d9956323898a8a798ad21901761143a4393dd
tianyilt <tianyilt@qq.com>
2019-12-21 12:29:18 +0800

    Create insert_origin.sql

---
+++ insert_origin.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : my
+Source Server Version : 50725
+Source Host           : 182.254.217.138:3306
+Source Database       : box_vs_sql
+
+Target Server Type    : MYSQL
+Target Server Version : 50725
+File Encoding         : 65001
+
+Date: 2019-12-19 16:33:32
+*/
+
+insert into login values
+(1,'tianyilt','123','mdzz'),
+(2,'alpha','123','md'),
+(3,'beta','123','mdz');
+
+
+insert into dlc values
+(1,1,1,20,'2019-12-03 18:00:01'),
+(2,2,1,20,'2019-12-03 20:00:01'),
+(3,1,2,20,'2019-12-03 18:00:01'),
+(4,1,3,20,'2019-12-03 20:00:01'),
+(5,5,1,20,'2019-12-03 20:00:01');
+
+insert into gamerecord values
+(1,1,1,'2019-12-04 18:00:01',20),
+(2,5,1,'2019-12-05 18:00:01',100),
+(3,3,2,'2019-12-06 18:00:01',21),
+(4,4,3,'2019-12-07 18:00:01',33);
