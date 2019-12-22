7c37638fdf720ad88eeaf416a424bba3eeec5e37
tianyilt <tianyilt@qq.com>
2019-12-22 19:15:49 +0800

    Update create_view.sql

--- create_view.sql
+++ create_view.sql
@@ -22,14 +22,14 @@ WHERE   (A2.Ascore = B.Score) AND (A2.UID = B.UID) AND (login.UID = B.UID)
 ORDER BY B.Score DESC
 
 
-
+/*死掉10个脑细胞的dashboard查询语句，自己用的是子查询，把用select出来的当做新表，命名为A2*/
 SELECT login.UserName,B.Score,B.EndTime
     from gamerecord B, (select A.UID,MAX(A.Score) AS Ascore
 from gamerecord A
 group by UID) A2, login
 WHERE A2.Ascore=B.Score AND A2.UID = B.UID AND login.UID=B.UID
 ORDER BY B.Score DESC;
-
+/*死掉微软脑细胞的数据表，由上面自动生成*/
 SELECT   login.UserName, B.Score, B.EndTime
 FROM      gamerecord AS B CROSS JOIN
                     (SELECT   A.UID, MAX(A.Score) AS Ascore
@@ -39,41 +39,10 @@ FROM      gamerecord AS B CROSS JOIN
 WHERE   (A2.Ascore = B.Score) AND (A2.UID = B.UID) AND (login.UID = B.UID)
 ORDER BY B.Score DESC
 
-select A.UID,MAX(A.Score) AS Ascore
-from gamerecord A
-group by UID
-
-
-
-select A.UID, A.Score, A.EndTime
-from gamerecord A,gamerecord B
-WHERE A.Score=B.Score;
-
-
-
-
-
+/*为查询令人深恶痛绝的username转uid did而准备的视图*/
 DROP view IF EXISTS `ID`;
 CREATE VIEW ID
 as
 SELECT   login.UserName, login.UID, dlc.DID, dlc.Dtype
 FROM login cross join dlc on dlc.UID = login.UID;
 
-SELECT   login.UserName, dlc.DID, dlc.Dtype
-FROM (login cross join dlc on dlc.UID = login.UID)
-    cross join gamerecord on gamerecord.UID=login.UID
-
-SELECT   login.UserName, login.UID, dlc.DID, dlc.Dtype
-FROM login cross join dlc on dlc.UID = login.UID;
-
-SELECT   gamerecord.DID, gamerecord.UID
-FROM gamerecord  right join dlc on gamerecord.uid = dlc.UID
-
-
-
-
-SELECT   UserName, MAX(EndTime) AS LastTime, MAX(Score) AS MaxScore
-FROM      gamerecord cross join login
-where gamerecord.UID = login.UID
-GROUP BY gamerecord.UID
-ORDER BY MAXSCORE DESC;
