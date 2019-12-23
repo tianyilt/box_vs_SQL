5307a336cb7536652bc85b96014df104d5206f84
tianyilt <tianyilt@qq.com>
2019-12-23 09:29:17 +0800

    修复不花钱就能用dlc的问题

--- themeselecter.py
+++ themeselecter.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from tkinter import *
 import tkinter.messagebox as tm
 import box
@@ -12,14 +13,41 @@ def ts(uid):
         ######################
         cd = accountopt()
         #purchase_flag ==1 则dlc已经购买
-        purchase_flag = 1
+        purchase_flag=0
+        import pymysql
+        db = pymysql.connect('182.254.217.138', 'ZNDY', 'ZNDY@ecust123', 'box_vs_sql', charset="utf8")
+        cursor = db.cursor()
+
+        sql = """SELECT Dtype FROM dlcbuy WHERE username = '%s'""" % (uid)
+        try:
+            cursor.execute(sql)  # 执行sql语句
+            results = cursor.fetchall()  # 获取查询的所有记录
+            # 返回值是一个元组的形式
+            if results:
+                for dlc_flag in results:
+                    if dlc_flag[0] == cd:#因为是元组，所以如果有一个购买记录的dtype和cd一样就ok
+                        purchase_flag = 1
+
+        except Exception as e:
+            db.rollback()
+
+        finally:
+            db.close()
+
+
+
+
+
+
+
+        #purchase_flag = 1#测试金手指
         if purchase_flag ==1:
             themesvc.destroy()
             box.box(cd,uid)
         else:
             tm.showerror("エラー","テーマは買いません")
             if tm.askyesno("メッセージ","テーマは買いますか？"):
-                dlc_pcs_svc.dlc()
+                dlc_pcs_svc.dlc(uid)
             else:
                 pass
     def accountopt():
